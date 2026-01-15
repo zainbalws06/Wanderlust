744ceb001f568a8ed217e270b32a7e0a3dacafac
Zain <zainbalws@gmail.com>
2026-01-15 14:43:31 +0500

    structurized

--- app.js
+++ app.js
@@ -9,10 +9,12 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const listingsRoutes = require("./routes/listingsRoutes");
 
-//Middlewares
+//SETTINGS
 app.set("view engine", "ejs");
 app.engine("ejs", ejsMate);
 app.set("views", path.join(__dirname, "views"));
+
+//GLOBAL MIDDLEWARES
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
@@ -37,25 +39,16 @@ app.use((err, req, res, next) => {
 });
 
 //START
-
-function listen() {
+async function start() {
   try {
+    await mongoose.connect(MONGO_URL);
+    console.log("connected to db");
+
     app.listen(PORT, () => {
       console.log("listening to the server");
     });
   } catch (err) {
-    console.log(err);
+    console.log(`connection to db failed ${err}`);
   }
 }
-listen();
-
-//DB Connection
-async function main() {
-  try {
-    await mongoose.connect(MONGO_URL);
-    console.log("connected to db");
-  } catch (error) {
-    console.log("connection to db failed");
-  }
-}
-main();
+start();
